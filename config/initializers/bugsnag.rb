f767856bb2888c6a3d94903ba4417e9c6d435517
樋浦 <mickydisney1212@gmail.com>
2022-11-16 09:39:13 +0900

    update README

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,3 +1,3 @@
-Bugsnag.configure do |config|
-  config.api_key = "e5afebffb1a8a7744ea95fd747bfbaf6"
-end
+# Bugsnag.configure do |config|
+#   config.api_key = "e5afebffb1a8a7744ea95fd747bfbaf6"
+# end
